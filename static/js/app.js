86c86e41a4549d8b885237ff987bb9ec72829b45
licia858 <licia858@yahoo.com>
2019-03-13 18:51:29 -0400

    hwupdate

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,7 @@
 // from data.js
 var tableData = data;
 var submit = d3.select("#filter-btn");
+var submit2 = d3.select("new-data-btn");
 
 //reference to the table body
 var tbody = d3.select("tbody");
@@ -28,50 +29,42 @@ tableData.forEach(function(ufoReport){
 submit.on("click", function() {
     console.log("taco");
 
-  // Prevent the page from refreshing
-    
+  // Prevent the page from refreshing 
   d3.event.preventDefault();
 
   // Select the input element and get the raw HTML node
   var inputElement = d3.select("#datetime");
 
-  // Get the value property of the input element
-  //grabs the data from the user
+  // Get the value property of the input element - grabs the data from the user
   var inputValue = inputElement.property("value");
 
-  console.log(inputValue);
-
+  console.log(inputValue); //print the input value
+  // find matching value from input tothe datetime
   var filteredData = tableData.filter(daterow => daterow.datetime === inputValue);
 
-     console.log(filteredData);
-
-
-
-  // Set the span tag in the h1 element to the text
-  // that was entered in the form
-  //d3.select("#ufo-table").text(filteredData);
-
-  //tryingsomethin
-  //$("tbody").remove();
+     console.log(filteredData); //print filteredData
+    //tryingsomething remove cleared the headers too. didnt work
+    //$("tbody").remove();
   var empty = d3.select("tbody");
   
-  empty.html(""); 
+  empty.html("");
+   
 
   var filterUFO = d3.select("tbody");
 
 //loop the data to console each element
    filteredData.forEach(function(ufodate){
      //console.log(ufoReport);
-
-
      //append on tablerow (tr) for each object
-    var row = filterUFO.append("tr");
+     var row = filterUFO.append("tr");
 
-    Object.entries(ufodate).forEach(function([key, value]){
+        Object.entries(ufodate).forEach(function([key, value]){
         //console.log(key, value);
         //append a cell to the row for each value
         var cell = filterUFO.append("td");
         cell.text(value);
+        });
     });
 });
-});
+
+
